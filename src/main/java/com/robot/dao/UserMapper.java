f10733cb6350a449c83387f95ebdc47db6799449
buzheng1949 <yupeibiaodeveloper@yeah.net>
2017-06-18 17:12:55 +0800

    feat:mark一下mybatis的配置

---
+++ src/main/java/com/robot/dao/UserMapper.java
@@ -0,0 +1,20 @@
+package com.robot.dao;
+
+import com.robot.model.User;
+
+
+public interface UserMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(User record);
+
+    int insertSelective(User record);
+
+    User selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(User record);
+
+    int updateByPrimaryKey(User record);
+
+//    int checkTotal()
+}
---
+++ src/main/webapp/WEB-INF/jsps/home.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: buzheng
+  Date: 17/6/18
+  Time: 下午12:36
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+
+</body>
+</html>
